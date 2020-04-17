4b3e24121a4fa117f51b65c09f0823413277f689
Jonathan Lebon <jonathan@jlebon.com>
2020-04-17 15:20:38 -0400

    print-rollout: support rollouts starting at 100%

--- print-rollout.py
+++ print-rollout.py
@@ -5,8 +5,13 @@ stream = update["stream"]
 release = update["releases"][-1]
 version = release["version"]
 rollout = release["metadata"]["rollout"]
-ts = datetime.datetime.fromtimestamp(rollout["start_epoch"])
-mins = rollout["duration_minutes"]
-hrs = mins / 60.0
 
-print(f"{stream} rollout of {version} scheduled for {ts} UTC for {mins}m ({hrs}h)")
+start_percentage = rollout["start_percentage"]
+# totally just going to ignore floating-point concerns here
+if int(start_percentage * 100) == 100:
+    print(f"{stream} rollout of {version} at 100%")
+else:
+    ts = datetime.datetime.fromtimestamp(rollout["start_epoch"])
+    mins = rollout["duration_minutes"]
+    hrs = mins / 60.0
+    print(f"{stream} rollout of {version} scheduled for {ts} UTC for {mins}m ({hrs}h)")
