80dc326a6eac990522a643219c0c2c83e747fa15
Luca BRUNO <luca.bruno@coreos.com>
2020-05-19 16:12:12 +0000

    print-rollout: take care of no rollouts case

--- print-rollout.py
+++ print-rollout.py
@@ -2,9 +2,17 @@ import json, sys, datetime
 
 update = json.load(open(sys.argv[1]))
 stream = update["stream"]
+releases = update.get("releases", None)
+if not releases:
+    print(f"{stream} has no rollouts")
+    sys.exit(0)
 release = update["releases"][-1]
 version = release["version"]
-rollout = release["metadata"]["rollout"]
+rollout = release["metadata"].get("rollout", None)
+if not rollout:
+    print(f"latest entry {version} on {stream} is not a rollout")
+    sys.exit(0)
+
 
 start_percentage = rollout["start_percentage"]
 # totally just going to ignore floating-point concerns here
